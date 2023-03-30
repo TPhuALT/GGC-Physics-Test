96f8eed89b0577390f16c4813d51681371236b6c
H4wkEyes <129346379+H4wkEyes@users.noreply.github.com>
2023-03-30 16:37:58 +0700

    lmao

---
+++ main1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>Goblins Go Codes</title>
+	<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">
+		<style>* 
+		{  box-sizing: border-box;}body {  margin: 0;}
+		/* Định dạng header */.header {  background-color: #00FFFF;  color: #58257b;  padding: 10px;  text-align: center;border-radius: 20px;}
+		/* Định dạng ảnh */.imageChange {max-width: 80px;height: auto;float : left;align-items: center;justify-content: center;}
+		/* Định dạng thanh điều hướng trên cùng */.topnav {  overflow: hidden;  background-color: #FFB681;height: 50px;border-radius: 20px;}
+		/* Định dạng username */.header a {  float: right;  display: block;  color: #000000;  text-align: center;  padding: 10px 20px;  text-decoration: none;}
+		/* Định dạng Đăng xuất */.header e { background-color: blue; float: right;  display: block;  color: #000000;  text-align: right;  padding: 10px 20px;  text-decoration: none;}
+		/* Định dạng link điều hướng */.topnav a {  float: left;  display: block;  color: #000000;  text-align: center;  padding: 14px 16px;  text-decoration: none;}
+		/* Thay đổi màu liên kết khi di chuột qua */.topnav a:hover {  background-color: #db7093;  color: white;  font-weight: bold}
+		/* Tạo ba cột không bằng nhau, float cạnh nhau */.column {  float: left;  padding: 10px;background-color:F9F9F8}
+		/* Cột bên trái và phải */.column.side {  width: 20%;}
+		/* Cột chính giữa */.column.middle {  width: 60%;}
+		/* Clear float khác sau các cột */.row:after {  content: "";  display: table;  clear: both;}
+		/* Bố cục linh hoạt: ba cột xếp chồng lên nhau thay vì cạnh nhau khi màn hình có chiều rộng dưới 600px */
+			@media screen and (max-width: 600px) {    .column.side, .column.middle {  width: 100%;  }}
+		</style>
+	</head>
+	<body>
+	<div class="row">
+	<div class="column side"> </div>
+	<div class="column middle">
+	<div class="header">
+	<h1><img src="img/logoQuyNhon-icon.jpg" alt="LOGO" class = imageChange></h1>
+	<h1>Ôn thi trắc nghiệm Vật Lí online<br> GGC </h1></div></div>
+	<div class="column side">
+	<a href = "#"><b><br><br><br><br><br><br>Xin chào username</b></a> 
+	
+	<a href = "#"><input type = "submit" style="background-color:#4176FF; border-color:white; color:white; border-radius: 20px;"name = "gui" value = "Đăng xuất"></</a>
+	</div></div>
+	<div class="row">
+	<div class="column side"> </div>
+	<div class="column middle">
+		<div class="topnav">  <a href="#"><b> Trang chủ  </b></a>
+			<a href="#"><b> Lớp 10 ▼ </b></a>  
+			
+
+			<a href="#"><b> Lớp 11 ▼ </b></a>
+			<a href="#"><b> Lớp 12 ▼ </b></a><a href="#"><b> Ôn thi tốt nghiệp ▼ </b></a>
+			<a href="#"><b> Tài liệu ▼</b></a>
+			<a href="#"><b> Liên hệ</b></a>
+		</div>
+	</div> 
+	<div class="column side">   </div></div>
+	<div class="row">
+	<div class="column side"></div>
+	<div class="column middle">    
+	<h2>Giới thiệu</h2>
+	
+	
+	
+	
+	<div class="column side"></div>
+	</div>
+	</body>
+</html>
+
